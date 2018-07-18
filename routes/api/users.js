5c6428965dd1b187bb02d53b9bd51dd455a1501c
Pranit Kumar Chandel <chandel.pranit@gmail.com>
2018-07-18 08:02:11 +0530

    Validation Handlers

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const key = require("../../config/keys");
 
+//load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 //load user Model
 const User = require("../../models/Users");
 //@route GET api/users/test
@@ -17,9 +20,16 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 //@access Public
 
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //Size
