5d75b4ee975241f9a5f5742d1da6aba36b691a05
Pranit Kumar Chandel <chandel.pranit@gmail.com>
2018-07-18 05:55:05 +0530

    User Registration Postman

--- server.js
+++ server.js
@@ -2,17 +2,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const app = express(); //initialize express
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
 
 //Connect to mongoDB
 mongoose
-  .connect(db)
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
